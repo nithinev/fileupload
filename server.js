7d08123665ee5052721588449e2cec923386ddb9
nithinev <nithinev1989@gmail.com>
2016-08-31 16:49:31 +0530

    xmlread

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ var express = require("express");
 var app     = express();
 var path    = require("path");
 var ejs = require('ejs');
+var fs = require('fs');
 var fileUpload = require('express-fileupload');
-
+var xml2js = require('xml2js');
 
 app.use(fileUpload());
 app.set('view engine', 'ejs');
@@ -21,15 +22,33 @@ app.post('/upload', function(req, res) {
     }
  
     sampleFile = req.files.sampleFile;
-    sampleFile.mv('filename.jpg', function(err) {
+    var path = req.files.sampleFile.path;
+    var name = req.files.sampleFile.name;
+    console.log(path+"="+name);	
+    sampleFile.mv(name, function(err) {
         if (err) {
             res.status(500).send(err);
         }
         else {
-            res.send('File uploaded!');
-        }
-    });
+            fs.readFile(name, 'utf8', function(err, contents) {
+		console.log(contents);
+			var xml = contents;
+			var parser = new xml2js.Parser();
+			parser.parseString(xml, function(err,result){
+			  //Extract the value from the data element
+			  //extractedData = result['opml']['body']['outline'];
+			  var title = result.opml.body[0].outline[0].$.title;
+			  var url = result.opml.body[0].outline[0].$.htmlUrl;			
+			  		
+			  fs.unlinkSync(name);
+			});
+	    });
+		console.log('after calling readFile');	
+	    }
+        });
 });
+    
+	
 
 var port = process.env.PORT || 8080;
 
